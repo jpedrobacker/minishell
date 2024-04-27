9cf9185215dbd35659e2333b686142d92a43eff5
PradoAllan <allanprado.it@gmail.com>
2024-04-27 18:28:39 -0300

    add: split env path

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:29:24 by jbergfel          #+#    #+#             */
-/*   Updated: 2024/04/27 17:22:07 by aprado           ###   ########.fr       */
+/*   Updated: 2024/04/27 18:23:53 by aprado           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,17 +45,40 @@ void	create_node(char *s, t_token **head)
 	current->next = new;
 }
 
-void	create_list(int ac, char **av, char **envp)
+char	*find_env_path(char **envp)
+{
+	char	*env_path;
+	int		i;
+
+	i = 0;
+	env_path = NULL;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			env_path = envp[i];
+			break ;
+		}
+		i++;
+	}
+	return (env_path);
+}
+
+void	create_list(char **av, char **envp)
 {
 	t_token	*head;
 	char	**splited;
+	char	**paths;
+	char	*env_path;
 	int		i;
 
-	(void)ac;
-	(void)envp;
 	i = 0;
 	head = NULL;
 	splited = ft_split(av[1], '|');
+	env_path = find_env_path(envp);
+	paths = ft_split((env_path + 5), ':');
+	ft_printf("env path: %s\n", env_path);
+	ft_printf("first path: %s\n", paths[0]);
 	while (splited[i])
 	{
 		create_node(splited[i], &head);
@@ -68,7 +91,7 @@ int	main(int ac, char **av, char **envp)
 {
 	if (ac != 2)
 		return (ft_putstr_fd("Missing parameters\n", 2), 0);
-	create_list(ac, av, envp);
+	create_list(av, envp);
 	return (1);
 }
 
