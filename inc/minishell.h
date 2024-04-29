6a03b153e8b182b5a733c42e2c1f9e2ecb599f5f
Allan <allanprado.it@gmail.com>
2024-04-29 15:55:48 -0300

    feat: add new prototypes

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 11:25:23 by jbergfel          #+#    #+#             */
-/*   Updated: 2024/04/27 18:50:24 by jbergfel         ###   ########.fr       */
+/*   Updated: 2024/04/29 15:25:18 by aprado           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,24 @@ typedef struct	s_token
 	int			fd_out;
 	char		*real_path;
 	char		*command;
+	char		*command_input;
 	char		**env_path;
 	char		**arr_command;
 	struct s_token	*next;
 }			t_token;
 
 void test_print(char *src);
+
+/*-- path functions --*/
+char	*find_env_path(char **envp);
+char	*divide_command_input(char *s);
+char	*get_real_path(char ***all_paths, char *command);
+
+/*-- linked list functions --*/
+void	create_node(char *s, t_token **head, char ***paths);
+void	create_list(char *usr_input, char **envp);
+
+/*-- utils --*/
+void	print_list(t_token **head);
 
 #endif
