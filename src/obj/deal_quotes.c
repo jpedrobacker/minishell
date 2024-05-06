c259583c1ccafae245f885d3c16e408676b7abe6
Allan <allanprado.it@gmail.com>
2024-05-06 13:58:17 -0300

    add: file with quotes func

--- src/obj/deal_quotes.c
+++ src/obj/deal_quotes.c
@@ -1,18 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   deal_quotes.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
+/*   By: aprado <aprado@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/23 11:29:24 by jbergfel          #+#    #+#             */
-/*   Updated: 2024/05/06 13:52:57 by aprado           ###   ########.fr       */
+/*   Created: 2024/05/06 13:51:06 by aprado            #+#    #+#             */
+/*   Updated: 2024/05/06 13:52:42 by aprado           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-/*
 int	change_pipe(char *s, int *start, int *end)
 {
 	int	change;
@@ -63,31 +62,4 @@ void	change_input(char *s)
 		}
 		i++;
 	}
-}
-*/
-
-int	main(int ac, char **av, char **envp)
-{
-	char	*usr_input;
-	(void) ac;
-	(void) av;
-	//(void)envp;
-
-	while (1)
-	{
-		usr_input = readline("minishell: ");
-		if (!usr_input)
-		{
-			ft_printf("Apertei cntrlD");
-			exit(EXIT_SUCCESS);
-		}
-		/*
-		if (ft_strncmp(usr_input, "cd", ft_strlen(usr_input)) == 0)
-			cd("");
-		*/
-		change_input(usr_input);
-		create_list(usr_input, envp);
-		free(usr_input);
-	}
-	return (1);
 }
