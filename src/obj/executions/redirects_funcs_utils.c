26f42dd5599027716db39f2185cb5ef38a441ec4
PradoAllan <allanprado.it@gmail.com>
2024-06-23 17:18:25 -0300

    feat: NOT DONE changing fds funcs

---
+++ src/obj/executions/redirects_funcs_utils.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirects_funcs_utils.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aprado <aprado@student.42.rio>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/23 16:22:56 by aprado            #+#    #+#             */
+/*   Updated: 2024/06/23 17:06:13 by aprado           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+//node->fd_in
+//node->fd_out
+void	heredoc_func(t_token *node, t_main *bag, int i)
+{
+	(void)bag;
+	(void)node;
+	(void)i;
+	ft_printf("EXECUTING HD\n");
+}
+
+void	append_func(t_token *node, t_main *bag, int i)
+{
+	(void)bag;
+	(void)node;
+	(void)i;
+	ft_printf("EXECUTING AP\n");
+}
+
+void	redirect_in(t_token *node, t_main *bag, int i)
+{
+	(void)bag;
+	if (!node->arr[i + 1])
+	{
+		//FREE NAS PARADA TODA
+		return ;
+	}
+	node->fd_in = open(node->arr[i + 1], O_RDONLY);
+	if (node->fd_in < 0)
+	{
+		//FREE NAS PARADA TODA
+		return (ft_putstr_fd("No such file or directory", 2));
+	}
+	if (dup2(node->fd_in, STDIN_FILENO) == -1)
+	{
+		//FREE NAS PARADA TODA
+		return (ft_putstr_fd("No such file or directory", 2));
+	}
+	//close(STDOUT_FILENO);
+}
+
+void	redirect_out(t_token *node, t_main *bag, int i)
+{
+	(void)bag;
+	if (!node->arr[i + 1])
+	{
+		//FREE NAS PARADA TODA
+		return ;
+	}
+	node->fd_in = open(node->arr[i + 1], O_CREAT | O_RDWR | O_TRUNC, 00700);
+	if (node->fd_in < 0)
+	{
+		//FREE NAS PARADA TODA
+		return (ft_putstr_fd("No such file or directory", 2));
+	}
+	if (dup2(node->fd_out, STDOUT_FILENO) == -1)
+	{
+		//FREE NAS PARADA TODA
+		return (ft_putstr_fd("No such file or directory", 2));
+	}
+}
